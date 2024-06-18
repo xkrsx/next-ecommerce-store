0390f6de6d733a334bb91f863053402e38ab161b
Jakub Markiewicz <markiewicz267@gmail.com>
2024-06-18 10:28:21 +0200

    Add listing of cart products with names/prices from DB

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,3 +1,40 @@
+import { getSingleProductInsecure } from '../../database/products';
+import { getCookie } from '../../util/cookies';
+import { parseJson } from '../../util/json';
+
 export default function Cart() {
-  return <h1>Cart</h1>;
+  const cartCountCookie = getCookie('cart');
+  const cartCounts = !cartCountCookie ? [] : parseJson(cartCountCookie);
+  let cartValue;
+
+  return (
+    <div className="cart-wrapper">
+      <h1>Cart</h1>
+      <div className="cart">
+        <ul>
+          {cartCounts.map(async (product) => {
+            const productInfo = await getSingleProductInsecure(
+              product.productId,
+            );
+            return (
+              <li key={`cart-product-${product.productId}`}>
+                name: {productInfo.name} count: {product.count} price per one:{' '}
+                {productInfo.price} total price:{' '}
+                {product.count * productInfo.price}
+              </li>
+            );
+          })}
+          total value: {cartCounts.map(async (product) => {
+            const productInfo = await getSingleProductInsecure(
+              product.productId,
+            );
+            return (
+              {}
+            )
+
+          }
+        </ul>
+      </div>
+    </div>
+  );
 }
