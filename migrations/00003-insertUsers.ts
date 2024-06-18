f4e27dff9679feed1e5b8405a688c626fa84abad
Jakub Markiewicz <markiewicz267@gmail.com>
2024-06-18 17:36:04 +0200

    Add migration: users table, insert fake users, orders table

---
+++ migrations/00003-insertUsers.ts
@@ -0,0 +1,51 @@
+import { Sql } from 'postgres';
+import { users } from '../database/usersExample';
+
+// TODO: generate fake users
+
+export async function up(sql: Sql) {
+  for (const user of users) {
+    await sql`
+      INSERT INTO
+        users (
+          role,
+          username,
+          first_name,
+          last_name,
+          mail,
+          phone,
+          street,
+          house_number,
+          apartment_number,
+          postcode,
+          city,
+          country
+        )
+      VALUES
+        (
+          ${user.role},
+          ${user.username},
+          ${user.firstName},
+          ${user.lastName},
+          ${user.mail},
+          ${user.phone},
+          ${user.street},
+          ${user.houseNumber},
+          ${user.apartmentNumber},
+          ${user.postcode},
+          ${user.city},
+          ${user.country}
+        )
+    `;
+  }
+}
+
+export async function down(sql: Sql) {
+  for (const user of users) {
+    await sql`
+      DELETE FROM users
+      WHERE
+        users.id = ${user.id}
+    `;
+  }
+}
