3688c1df0b8906a40c72274e1295c7ad7db67ec6
Jakub Markiewicz <markiewicz267@gmail.com>
2024-06-19 16:18:22 +0200

    [WiP] Add styling to cart page

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,10 +1,8 @@
-// import Image from 'next/image';
-
+import './page.scss';
 import Link from 'next/link';
 import { getSingleProductInsecure } from '../../database/products';
 import { getCookie } from '../../util/cookies';
 import { parseJson } from '../../util/json';
-import { createOrUpdateCookie } from '../common/AddToCart/actions';
 import { cartCalculator } from './actions';
 
 // TODO: add style
@@ -19,13 +17,11 @@ export default async function Cart() {
     return acc + Number(product.price) * Number(product.count);
   }, 0);
 
-  console.log(totalValue);
-
   return (
     <div className="cart-wrapper">
       <h1>Cart</h1>
       <div className="cart">
-        <ul>
+        <ul className="cart-product">
           {cart.map(async (product) => {
             const productInfo = await getSingleProductInsecure(
               product.productId,
@@ -34,48 +30,85 @@ export default async function Cart() {
 
             return (
               <li key={`cart-product-${product.productId}`}>
-                <ul>
+                <ul className="product-info">
                   {/* <Image
                     src={`/images/products/${product.name}/1.webp`}
                     alt={product.name}
                     layout="fill"
                     objectFit="contain"
                   /> */}
-                  <li>name: {productInfo.name}</li>
-                  <li>count: {product.count}</li>
-                  <li>price per one: {productInfo.price}</li>
-                  <li> total price: {singleProductValue}</li>
+                  <li>{productInfo.name}</li>
                   <li>
-                    <button>-1</button>
-                    <button
-                      formAction={createOrUpdateCookie(
-                        productInfo.id,
-                        product.count + 1,
-                      )}
+                    <span className="quantity-label">Quantity:</span>
+                    <div className="quantity-input-buttons">
+                      <button
+                        className="change-quantity-button decrease"
+                        // style={{
+                        //   cursor: !cartQuantity ? 'not-allowed' : 'pointer',
+                        // }}
+                        // disabled={!cartQuantity}
+                        // onClick={handleQuantityChange}
+                        name="decreaseQuantity"
+                      >
+                        -
+                      </button>
+
+                      <input
+                        className="quantity-input"
+                        name="changeQuantityInput"
+                        type="number"
+                        disabled
+                        min="0"
+                        // max={stockCount}
+                        // value={cartQuantity}
+                      />
+
+                      <button
+                        className="change-quantity-button increase"
+                        // onClick={handleQuantityChange}
+                        // disabled={cartQuantity >= stockCount}
+                        // style={{
+                        //   cursor:
+                        //     cartQuantity >= stockCount
+                        // ? 'not-allowed'
+                        // : 'pointer',
+                        // }}
+                        name="increaseQuantity"
+                      >
+                        +
+                      </button>
+                    </div>
+
+                    {/* TODO add cookie functionality to -/+ buttons instead */}
+                    {/* <button
+                      disabled={!cartQuantity}
+                      style={{
+                        cursor: !cartQuantity ? 'not-allowed' : 'pointer',
+                      }}
+                      // formAction={async () =>
+                      //   await createOrUpdateCookie(productId, cartQuantity)
+                      }
                       className="add-to-cart-button"
                     >
-                      +1
-                    </button>
+                      Add to cart
+                    </button> */}
                   </li>
+                  <li>price per one: {productInfo.price}€</li>
+                  <li> total price: {singleProductValue}€</li>
+
                   <li>
-                    <button>remove</button>
+                    <button className="remove-button">Remove product</button>
                   </li>
                 </ul>
               </li>
             );
           })}
         </ul>
-        <hr />
-        total value: {totalValue}
-        <hr />
-        <div
-          style={{
-            borderStyle: '1px solid black',
-            backgroundColor: 'lightcyan',
-            borderRadius: '0.5rem',
-          }}
-        >
-          <Link href="/checkout">Checkout</Link>
+        <div className="value-link">
+          <div className="total-value">total value: {totalValue}€</div>
+          <div className="checkout-link">
+            <Link href="/checkout">Checkout</Link>
+          </div>
         </div>
       </div>
     </div>
